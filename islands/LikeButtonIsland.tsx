be99871ecd715aea6015598c74d4f44672f9b8f5
Brown <rafael30011@gmail.com>
2024-04-13 17:42:53 -0300

    finish task 10 e 11,12,13,14,15,16

--- islands/LikeButtonIsland.tsx
+++ islands/LikeButtonIsland.tsx
@@ -5,6 +5,7 @@ import { total } from "$sdk/useTotalLikes.ts";
 import { useEffect } from "preact/hooks";
 import { SendEventOnClick } from "deco-sites/brown-deco-camp/components/Analytics.tsx";
 import { useId } from "deco-sites/brown-deco-camp/sdk/useId.ts";
+import { Bounce, toast, ToastContainer } from "react-toastify";
 
 export interface LikeButtonIslandProps {
   productID: string;
@@ -14,6 +15,8 @@ function LikeButtonIsland({ productID }: LikeButtonIslandProps) {
   const selected = useSignal(false);
   const quantity = useSignal(0);
   const id = useId();
+
+  const Toast = ToastContainer as any;
 
   useEffect(() => {
     const updateTotals = async () => {
@@ -41,35 +44,51 @@ function LikeButtonIsland({ productID }: LikeButtonIslandProps) {
     total.value = totalLikes.total;
     const totalLikesProduct = await invoke["deco-sites/brown-deco-camp"].loaders.totalLikesProductLoader({ productID });
     quantity.value = totalLikesProduct.product;
+
+    toast.success("👍 Curtiu meeeeeu!", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
   };
 
   return (
-    <button
-      id={id}
-      class="absolute left-4 sm:left-auto sm:right-4 top-4 flex items-center justify-center gap-1 p-1 sm:p-2 rounded bg-neutral sm:bg-white min-w-14"
-      onClick={(e) => handleToggleLike(e)}
-    >
-      {!selected.value ? (
-        <Icon id="MoodSmile" width={24} height={24} />
-      ) : (
-        <Icon id="MoodCheck" width={24} height={24} />
-      )}
-      <span class={`min-w-4 text-center text-xs font-thin ${!selected.value ? "text-gray-500" : "text-secondary"}`}>
-        {quantity.value}
-      </span>
-
-      <SendEventOnClick
+    <>
+      <button
         id={id}
-        event={{
-          name: "post_score",
-          params: {
-            score: quantity.value + 1,
-            level: 5,
-            character: String(productID),
-          },
-        }}
-      />
-    </button>
+        class="absolute left-4 sm:left-auto sm:right-4 top-4 flex items-center justify-center gap-1 p-1 sm:p-2 rounded bg-neutral sm:bg-white min-w-14"
+        onClick={(e) => handleToggleLike(e)}
+      >
+        {!selected.value ? (
+          <Icon id="MoodSmile" width={24} height={24} />
+        ) : (
+          <Icon id="MoodCheck" width={24} height={24} />
+        )}
+        <span class={`min-w-4 text-center text-xs font-thin ${!selected.value ? "text-gray-500" : "text-secondary"}`}>
+          {quantity.value}
+        </span>
+
+        <SendEventOnClick
+          id={id}
+          event={{
+            name: "post_score",
+            params: {
+              score: quantity.value + 1,
+              level: 5,
+              character: String(productID),
+            },
+          }}
+        />
+      </button>
+
+      <Toast />
+    </>
   );
 }
 
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,9 +4,7 @@ import { Context } from "deco/deco.ts";
 import Theme from "../sections/Theme/Theme.tsx";
 
 const sw = () =>
-  addEventListener("load", () =>
-    navigator && navigator.serviceWorker &&
-    navigator.serviceWorker.register("/sw.js"));
+  addEventListener("load", () => navigator && navigator.serviceWorker && navigator.serviceWorker.register("/sw.js"));
 
 export default defineApp(async (_req, ctx) => {
   const revision = await Context.active().release?.revision();
@@ -22,10 +20,9 @@ export default defineApp(async (_req, ctx) => {
         <meta name="view-transition" content="same-origin" />
 
         {/* Tailwind v3 CSS file */}
-        <link
-          href={asset(`/styles.css?revision=${revision}`)}
-          rel="stylesheet"
-        />
+        <link href={asset(`/styles.css?revision=${revision}`)} rel="stylesheet" />
+
+        <link rel="stylesheet" href="https://esm.sh/react-toastify@9.1.1/dist/ReactToastify.css" />
 
         {/* Web Manifest */}
         <link rel="manifest" href={asset("/site.webmanifest")} />
@@ -35,10 +32,7 @@ export default defineApp(async (_req, ctx) => {
       <ctx.Component />
 
       {/* Include service worker */}
-      <script
-        type="module"
-        dangerouslySetInnerHTML={{ __html: `(${sw})();` }}
-      />
+      <script type="module" dangerouslySetInnerHTML={{ __html: `(${sw})();` }} />
     </>
   );
 });
